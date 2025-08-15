c136d1c8a44ce45284acf4b831770ea38ef67207
Yi Zhou <yi.zhou@sansano.usm.cl>
2025-08-14 22:43:03 -0400

    refactor: minor changes

--- frontend/src/api/posts.ts
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-export async function fetchPosts() {
-    const KEYWORDS = 'chile'
-    const SORT_TYPE= 'recent'
-    const TIME_TYPE= 'all'
-    const posts = await axios.get(`https://www.reddit.com/search.json?q=${KEYWORDS}&restrict_sr=false&sort=${SORT_TYPE}&t=${TIME_TYPE}`)
-    return posts.data.data
-}
